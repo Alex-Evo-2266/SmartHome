51d2ebc71ad6d807a01026e4ec49624d5ec3e96e
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2021-01-23 21:11:03 +0300

    first commit

---
+++ SmartHome/smarthome/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmarthomebaseConfig(AppConfig):
+    name = 'smartHomeBase'
---
+++ SmartHome/smarthome/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    UserName = models.CharField("user name", max_length = 200)
+    UserPassword = models.CharField("user password", max_length = 200)
+    UserEmail = models.EmailField("user email")
+    UserMobile = models.CharField("user mobile", max_length = 200)
+    UserLevel = models.IntegerField("user level")
+
+class Device(models.Model):
+    DeviceName = models.CharField("device name", max_length = 200)
+    DeviceSystemName = models.CharField("device system name", max_length = 200)
+    DeviceInformation = models.TextField("user config")
+    DeviceTypeConnect = models.CharField("device type connect", max_length = 200)
+    DeviceType = models.CharField("device type", max_length = 200)
