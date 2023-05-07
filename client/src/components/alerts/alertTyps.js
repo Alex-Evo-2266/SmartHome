beb810aeb0948dc361f5e4f446a292401171d6f7
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-05-07 15:55:17 +0300

    new client init

--- client/src/components/alerts/alertTyps.js
+++
@@ -1,4 +0,0 @@
-export const INFO = "info"
-export const ERROR = "error"
-export const WARNING = "warning"
-export const SUCCESS = "success"
--- newclient/types/app.d.ts
+++
@@ -1,8 +0,0 @@
-
-declare global {
-    declare type RootState = import('../src/app/appStore').RootState
-    declare type AppDispatch = import('../src/app/appStore').AppDispatch
-    declare type AppStore = import('../src/app/appStore').AppStore
-  }
-
-export {}
