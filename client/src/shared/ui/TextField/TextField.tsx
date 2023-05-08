87cd1571bd2bf8134efe3b94b9afdef92469d229
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-05-08 15:43:31 +0300

    text field with icon

--- client/src/shared/ui/TextField/TextField.tsx
+++ client/src/shared/ui/TextField/TextField.tsx
@@ -1,6 +1,8 @@
+import { XCircle } from "lucide-react"
 import "./textField.scss"
+import { useRef } from "react"
 
-interface textFieldProps{
+interface ITextFieldProps{
     onChange?:(event: React.ChangeEvent<HTMLInputElement>)=>void
     name?: string
     value?: number | string
@@ -10,9 +12,13 @@ interface textFieldProps{
     onFocus?: (event:React.FocusEvent<HTMLInputElement>)=>void
     onBlur?: (event:React.FocusEvent<HTMLInputElement>)=>void
     error?: boolean
+    icon?:React.ReactNode
+    onClear?: ()=>void
 }
 
-export const TextField:React.FC<textFieldProps> = ({onChange, name, value, placeholder, className, validEmptyValue, onFocus, onBlur, error}) => {
+export const TextField = ({onClear, icon, onChange, name, value, placeholder, className, validEmptyValue, onFocus, onBlur, error}:ITextFieldProps) => {
+
+    const inputElement = useRef<HTMLInputElement>(null)
 
     const emptyValueClass = (validEmptyValue?:boolean, value?: string | number) => {
         if(error)
@@ -22,19 +28,38 @@ export const TextField:React.FC<textFieldProps> = ({onChange, name, value, place
         return ""	
     }
 
+    const focus = () => {
+        if(!inputElement.current)
+            return
+        inputElement.current.focus()
+    }
+
     return(
         <div className={`text-field`}>
-			<input 
-            required 
-            type="text" 
-            className={`${className} ${emptyValueClass(validEmptyValue, value)}`} 
-            name={name} 
-            value={value} 
-            onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}/>
-			<label>{placeholder}</label>
+            {
+                (icon)?
+                <div className="icon-container" onClick={focus}>{icon}</div>:
+                null
+            }
+            <div className="input-container" onClick={focus}>
+                <input
+                ref={inputElement}
+                required 
+                type="text" 
+                className={`${className} ${emptyValueClass(validEmptyValue, value)}`} 
+                name={name} 
+                value={value} 
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}/>
+                <label>{placeholder}</label>
+            </div>
             <span className="text-field-line"></span>
+            {
+                (onClear)?
+                <div className="clear-container"><XCircle onClick={onClear}/></div>:
+                null
+            }
 		</div>
     )
 }
