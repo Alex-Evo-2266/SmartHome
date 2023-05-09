97501f04c16d977b1edfb28cab614653383a02fd
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-05-09 10:27:09 +0300

    button blick

--- client/src/shared/ui/Button/Button.tsx
+++ client/src/shared/ui/Button/Button.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import "./button.scss"
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
@@ -7,4 +6,25 @@ export const BorderButton= (props: ButtonProps) => Button({...props, className:(
 
 export const MinButton = (props: ButtonProps) => Button({...props, className:(props.className ?? "") + " min"})
 
-export const Button = (props: ButtonProps) => <button {...{...props, className:(props.className ?? "") + " btn"}}/>
+export const Button = (props: ButtonProps) => {
+
+    const click = (e:React.MouseEvent<HTMLButtonElement>) => {
+        props.onClick && props.onClick(e)
+        let overlay = document.createElement('span')
+        overlay.classList.add("btn-overlay")
+        let x = e.clientX - e.currentTarget.offsetLeft
+        let y = e.clientY - e.currentTarget.offsetTop
+        overlay.style.left = x + "px"
+        overlay.style.top = y + "px"
+        e.currentTarget.appendChild(overlay)
+
+        setTimeout(()=>{
+            overlay.remove()
+        },500)
+    }
+
+    return(
+    <button {...{...props, className:(props.className ?? "") + " btn", onClick: click}}>
+    </button>
+    )
+}
--- client/src/shared/ui/IconButton/IconButton.tsx
+++ client/src/shared/ui/IconButton/IconButton.tsx
@@ -9,10 +9,28 @@ interface IconButtonProps{
     disabled?: boolean
   }
 
-export const IconButton = ({icon, className, onClick, onContextMenu, disabled}: IconButtonProps) => (
-    <button className={`iconbutton`} onClick={onClick} onContextMenu={onContextMenu} disabled={disabled}>
-        <div className={`${className} iconbutton-container`}>
-            {icon}
-        </div>
-    </button>
-)
+export const IconButton = ({icon, className, onClick, onContextMenu, disabled}: IconButtonProps) => {
+
+    const click = (e:React.MouseEvent<HTMLButtonElement>) => {
+        onClick && onClick(e)
+        let overlay = document.createElement('span')
+        overlay.classList.add("btn-overlay")
+        let x = e.clientX - e.currentTarget.offsetLeft
+        let y = e.clientY - e.currentTarget.offsetTop
+        overlay.style.left = x + "px"
+        overlay.style.top = y + "px"
+        e.currentTarget.appendChild(overlay)
+
+        setTimeout(()=>{
+            overlay.remove()
+        },500)
+    }
+
+    return(
+        <button className={`iconbutton`} onClick={click} onContextMenu={onContextMenu} disabled={disabled}>
+            <div className={`${className} iconbutton-container`}>
+                {icon}
+            </div>
+        </button>
+    )
+}
