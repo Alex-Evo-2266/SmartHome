489f9a86ef9bb9bf4c3b194a6b4e74fb642e79ef
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-05-21 16:41:06 +0300

    selection dialog

---
+++ client/src/shared/ui/Dialog/BaseDialog/SelectionDialog.tsx
@@ -0,0 +1,64 @@
+import { useCallback, useState } from "react"
+import { BasicTemplateDialog, TextButton } from "../.."
+import { ListContainer } from "../../List/List"
+import { ListItem } from "../../List/ListItem"
+import { BaseRadioButton, RadioButton } from "../../RadioButton/RadioButton"
+import { Divider } from "../../Divider/Divider"
+
+interface IItem<T>{
+    title: string
+    data: T
+}
+
+interface BaseDialogButtonProps{
+    onSuccess?: ()=>void
+    onHide?: ()=>void
+    disabled?: boolean
+}
+
+interface SelectionDialogProps<T>{
+    onSuccess?: (data:T)=>void
+    items: IItem<T>[]
+    text?: string
+    header: string
+    onHide?: ()=>void
+    name?: string
+}
+
+export function SelectionDialog<T>({onSuccess, items, text, header, onHide, name="dailog_name"}:SelectionDialogProps<T>) {
+
+    const [value, setValue] = useState<T | undefined>(undefined)
+
+    const success = useCallback(() => {
+        onSuccess && value && onSuccess(value)
+        onHide && onHide()
+    },[value])
+
+    const change = useCallback((data: T) => {
+        setValue(data)
+    },[])
+    return(
+        <BasicTemplateDialog header={header} action={<BaseDialogButton onHide={onHide} onSuccess={success} disabled={value === undefined}/>}>
+            <Divider/>
+            <ListContainer className="transparent" scroll maxHeight="200px">
+                {
+                    items.map((item, index)=>(
+                        <label key={index}>
+                            <ListItem hovered header={item.title} onClick={()=>change(item.data)} control={<BaseRadioButton name={name} checked={item.data === value}/>}/>  
+                        </label>
+                    ))
+                }
+            </ListContainer>
+            <Divider/>
+        </BasicTemplateDialog>
+    )
+}
+
+function BaseDialogButton({onSuccess, onHide, disabled}:BaseDialogButtonProps){
+    return(
+        <div className="dialog-button-container">
+            <TextButton onClick={onHide}>cancel</TextButton>
+            <TextButton onClick={onSuccess} disabled={disabled}>OK</TextButton>
+        </div>
+    )
+}
