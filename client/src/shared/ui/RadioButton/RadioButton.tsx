8017b5220ea0a8225bcea9ab9a22d8853ddfcd21
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-05-21 16:20:11 +0300

    base radio button

--- client/src/shared/ui/RadioButton/RadioButton.tsx
+++ client/src/shared/ui/RadioButton/RadioButton.tsx
@@ -6,13 +6,29 @@ interface RadioButtonProps{
     value?: string | number
     onChange?: (e:React.ChangeEvent<HTMLInputElement>)=>void
     readOnly?: boolean
+    checked?: boolean
 }
 
-export const RadioButton = ({name, currentValue, value, onChange, readOnly}:RadioButtonProps) => {
+interface BaseRadioButtonProps{
+    name: string
+    checked?: boolean
+}
+
+export const BaseRadioButton = ({name, checked}:BaseRadioButtonProps) => {
 
     return(
         <label className="radio-button-container">
-            <input className="radio-button" name={name} type="radio" checked={currentValue === value} value={value} onChange={onChange} readOnly={readOnly}/>
+            <input className="radio-button" name={name} type="radio" checked={checked} readOnly/>
+            <span></span>
+        </label>
+    )
+}
+
+export const RadioButton = ({name, currentValue, value, onChange, readOnly, checked}:RadioButtonProps) => {
+
+    return(
+        <label className="radio-button-container">
+            <input className="radio-button" name={name} type="radio" checked={currentValue === value || checked} value={value} onChange={onChange} readOnly={readOnly}/>
             <span></span>
         </label>
     )
