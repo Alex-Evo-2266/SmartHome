379f26495d1024fa06e17aee7ad37b04f1fc8899
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-06-02 19:17:50 +0300

    divider text

--- client/src/shared/ui/Divider/Divider.tsx
+++ client/src/shared/ui/Divider/Divider.tsx
@@ -2,9 +2,20 @@ import "./Divider.scss"
 
 interface DividerProps{
     short?: boolean
+    text?: string
 }
 
-export const Divider = ({short}:DividerProps) => {
+export const Divider = ({short, text}:DividerProps) => {
+
+    if(text)
+        return(
+            <div className={`divider-and-text ${short?"short":""}`}>
+                <div className={`divider ${short?"short":""}`}></div>
+                {text}
+                <div className={`divider ${short?"short":""}`}></div>
+            </div>
+        )
+
     return(
         <div className={`divider ${short?"short":""}`}></div>
     )
