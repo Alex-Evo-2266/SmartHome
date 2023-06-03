046c095a1679cd7254ed3b1610fc960540ec8882
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-06-03 15:39:48 +0300

    dict

--- client/types/app.d.ts
+++ client/types/app.d.ts
@@ -3,6 +3,12 @@ declare global {
     declare type RootState = import('../src/app/store/appStore').RootState
     declare type AppDispatch = import('../src/app/store/appStore').AppDispatch
     // declare type AppStore = import('../src/app/store/appStore').AppStore
+
+    interface IDict{
+      [key:string]: string
+    }
+
+    declare type Dict = IDict
   }
 
 export {}
