0bb63d293f3f5195d10883076aa16415de40a7b9
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-06-03 15:47:21 +0300

    auth page

---
+++ client/src/pages/Auth/ui/page.tsx
@@ -0,0 +1,11 @@
+import { AuthForm } from "../../../widgets/AuthForm"
+import './AuthPage.scss'
+
+export const AuthPage = () => {
+
+    return(
+        <div className="auth-page">
+            <AuthForm/>
+        </div>
+    )
+}
