44a3a22555918b838b3d9d9ccf914defc77d513f
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-06-04 10:48:39 +0300

    init nav item

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,12 +2,20 @@ import { BrowserRouter } from 'react-router-dom';
 import InitModalComponents from './InitModalComponents';
 import { useRoutes } from './routs';
 import { useAppSelector } from '../shared/lib/hooks/redux';
+import { useEffect } from 'react';
+import { NavigationInit } from '../features/Navigation';
 
 function App() {
 
 	const user = useAppSelector(state=>state.auth)
+	const {init} = NavigationInit()
 
 	const route = useRoutes(user.isAuthenticated)
+
+	useEffect(()=>{
+		if(user.isAuthenticated)
+			init()
+	},[user.isAuthenticated, init])
 
 	return (
 		<div className="App">
