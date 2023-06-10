d535ad30d15674715f368ad58dcca182f4f8a3da
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2023-06-10 10:48:42 +0300

    settings page

--- client/src/pages/Settings/ui/SettingsPage.tsx
+++ client/src/pages/Settings/ui/SettingsPage.tsx
@@ -1,14 +1,30 @@
+import { UserRole } from '../../../entites/User'
+import { useAppSelector } from '../../../shared/lib/hooks/redux'
 import { SettingsNavigationTable } from '../../../widgets/NavigationSettings'
+import { ProfileSettings } from '../../../widgets/ProfileSettings'
 import { ServerSettings } from '../../../widgets/ServerSettings'
 import './SettingsPage.scss'
 
 export const SettingsPage = () => {
 
+    const auth = useAppSelector(state=>state.auth)
 
     return(
         <div className="settings-page-container">
-            <SettingsNavigationTable/>
-            <ServerSettings/>
+            <div className='navigete-settings'>
+                <SettingsNavigationTable />
+            </div>
+            {
+                (auth.role === UserRole.ADMIN)?
+                <div className='server-settings'>
+                    <ServerSettings/>
+                </div>:
+                null
+            }
+            <div className='profile-settings'>
+                <ProfileSettings />
+            </div>
+            {/* <Profile className='profile-settings'/> */}
         </div>
     )
 }
