cf3a52433c4bcb35b0a94112cf506c4dd4e5ad2f
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2025-08-30 14:24:48 +0300

    script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ sh $BASE_DIR/scripts/create_network.sh
 
 sh $BASE_DIR/scripts/copy_shared_lib.sh
 
-sudo docker compose --env-file $BASE_DIR/.env -f $BASE_DIR/docker-compose-traefik.yml -f $BASE_DIR/AuthService/docker-compose-auth.yml build
+docker compose --env-file $BASE_DIR/.env -f $BASE_DIR/docker-compose-traefik.yml -f $BASE_DIR/AuthService/docker-compose-auth.yml build
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 
 BASE_DIR="$(dirname "$(realpath "$0")")/.."
 
-sudo docker compose --env-file $BASE_DIR/.env --file $BASE_DIR/docker-compose-traefik.yml start
+docker compose --env-file $BASE_DIR/.env --file $BASE_DIR/docker-compose-traefik.yml start
 
