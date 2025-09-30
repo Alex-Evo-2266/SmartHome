410d54fbe0e204f43e02daac864e153882ff8a25
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2025-09-30 20:32:36 +0300

    manager

--- ModuleManeger/server/app/configuration/settings.py
+++ ModuleManeger/server/app/configuration/settings.py
@@ -12,3 +12,8 @@ from app.configuration.settings_prod import *
 ORIGINS = ["localhost",'127.0.0.1','192.168.0.9','192.168.0.4']
 
 ROUTE_PREFIX = "/api-modules-manager"
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+CONFIG_SERVICES_DIR = os.path.join(BASE_DIR, "config_services") 
+MODULES_DIR = os.path.join(BASE_DIR, "modules")
--- ModuleManeger/server/app/internal/module/routes/modules.py
+++ ModuleManeger/server/app/internal/module/routes/modules.py
@@ -1,12 +1,30 @@
-import json, logging
+import json, logging, yaml, os
 from typing import Optional, List
 
-from app.configuration.settings import ROUTE_PREFIX
+from app.configuration.settings import ROUTE_PREFIX, MODULES_DIR
 
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
 logger = logging.getLogger(__name__)
+
+def load_module_configs(modules_dir: str):
+    configs = []
+    for root, dirs, files in os.walk(modules_dir):
+        if "module_config.yml" in files:
+            config_path = os.path.join(root, "module_config.yml")
+            with open(config_path, "r", encoding="utf-8") as f:
+                try:
+                    config = yaml.safe_load(f)
+                except yaml.YAMLError as e:
+                    print(f"Ошибка чтения {config_path}: {e}")
+                    continue
+            configs.append({
+                "module": os.path.basename(root),
+                "path": root,
+                "config": config
+            })
+    return configs
 
 router = APIRouter(
 	prefix=f"{ROUTE_PREFIX}/modules",
@@ -17,6 +35,7 @@ router = APIRouter(
 @router.get("/all")
 async def get_role():
 	try:
-		return "ok"
+
+		return load_module_configs(MODULES_DIR)
 	except Exception as e:
 		return JSONResponse(status_code=400, content=str(e))
