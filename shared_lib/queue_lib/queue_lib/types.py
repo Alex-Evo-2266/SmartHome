ac22553330e12ca31971c3392a485d0b9883c736
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2025-11-06 11:46:19 +0300

    libs

--- shared_lib/queue_lib/queue_lib/types.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class QueueItem(BaseModel):
-    type: str
-
-    class Config:
-        use_enum_values = True
--- shared_lib/rabitmq/rabitmq/__init__.py
+++
@@ -1,3 +0,0 @@
-from .consumer import QueueConsumer, FanoutConsumer
-from .producer import RabbitMQProducer, RabbitMQProducerFanout
-from .sender import FanoutSender, QueueSender
