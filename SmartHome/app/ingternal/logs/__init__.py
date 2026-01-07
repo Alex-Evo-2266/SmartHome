6533f8e78596a69d6a529e43afc6e1eccae78c60
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-01-07 13:05:58 +0300

    logs

--- SmartHome/app/ingternal/logs/__init__.py
+++ SmartHome/app/ingternal/logs/__init__.py
@@ -1,14 +1,21 @@
-from app.ingternal.logs.handlers import handler_base,handler_loop, handler_test, handler_listener, handler_sender, handler_polling,handler_room, handler_automatization, handler_device_crud, handler_device_base_calss, handler_device_save
-from app.ingternal.logs.logs import MyLogger
+from app.ingternal.logs.get_logger import get_logger
 
-get_base_logger = MyLogger(handler_base)
-get_polling_logger = MyLogger(handler_polling)
-get_automatization = MyLogger(handler_automatization)
-get_device_crud = MyLogger(handler_device_crud)
-get_device_base_class = MyLogger(handler_device_base_calss)
-get_device_save = MyLogger(handler_device_save)
-get_room_logger = MyLogger(handler_room)
-get_sender_logger = MyLogger(handler_sender)
-get_listener_logger = MyLogger(handler_listener)
-get_test_logger = MyLogger(handler_test)
-get_loop_logger = MyLogger(handler_loop)
+class MyLogger:
+    def __init__(self, file_name: str = None):
+        self.file_name = file_name 
+
+    def get_logger(self, name: str):
+        return get_logger(name, self.file_name or name)
+
+get_base_logger = MyLogger()
+get_polling_logger = MyLogger()
+get_automatization = MyLogger()
+get_device_crud = MyLogger()
+get_device_base_class = MyLogger()
+get_device_save = MyLogger()
+get_room_logger = MyLogger()
+get_sender_logger = MyLogger()
+get_listener_logger = MyLogger()
+get_test_logger = MyLogger()
+get_loop_logger = MyLogger()
+get_queue = MyLogger()
